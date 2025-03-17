3d61e0760732b42b7f9bdcb442602d65c5793d6b
Rachit <rachitdubey37@gmail.com>
2025-03-17 19:34:37 +0530

    Refactor logoutUser to use $unset for clearing refreshToken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -160,7 +160,7 @@ const loginUser = asyncHandler(async (req, res) => {
 const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
-    { $set: { refreshToken: undefined } },
+    { $unset: { refreshToken: 1 } }, //this removes the feild from document
     { new: true }
   );
 
